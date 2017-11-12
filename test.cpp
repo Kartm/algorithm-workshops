e2aa55a1a854942aabc65cb4ac6eb2b948363684
Łukasz Blachnicki <superkwek0@gmail.com>
2017-11-12 02:38:31 +0100

    A lot of work is needed here...

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -11,6 +12,9 @@ int main()
     cout << "Set the amount of edges: ";
     int edges; cin >> edges;
     vector<vector<pair<int, int> > > graph (nodes + 1);
+    vector<int> distanceFromSource (nodes + 1, -1);
+    vector<bool> visited (nodes + 1, false);
+
     cout << "Enter the nodes. [node] [neighbor] [cost] e. g. \"1 3 5\": \n";
     for(int i = 0; i < edges; i++)
     {
@@ -18,6 +22,21 @@ int main()
         cin >> a >> b >> cost;
         graph[a].push_back(make_pair(b, cost));
         graph[b].push_back(make_pair(a, cost));
+    }
+    cout << "\nEnter the source node: ";
+    int source; cin >> source;
+
+    ///Now the Dijkstra is coming
+    distanceFromSource[source] = 0;
+    priority_queue<pair<int, int> > theOrder;
+    theOrder.push(make_pair((-1) * distanceFromSource[source], source));
+    while(!theOrder.empty())
+    {
+        pair<int, int> currentQueueElement = theOrder.top();
+        int currentNode = currentQueueElement.second;
+        int currentCost = currentQueueElement.first;
+
+        ///get the minimum
     }
 
     return 0;
