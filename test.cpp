6ffbeff3336cc80f3c1d16db58eeff4c1e8946da
Łukasz Blachnicki <superkwek0@gmail.com>
2017-11-12 02:21:33 +0100

    Basic code.

--- test.cpp
+++ test.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    
+    cout << "Set the amount of nodes: ";
+    int nodes; cin >> nodes;
+    cout << "Set the amount of edges: ";
+    int edges; cin >> edges;
+    vector<vector<pair<int, int> > > graph (nodes + 1);
+    cout << "Enter the nodes. [node] [neighbor] [cost] e. g. \"1 3 5\": \n";
+    for(int i = 0; i < edges; i++)
+    {
+        int a, b, cost;
+        cin >> a >> b >> cost;
+        graph[a].push_back(make_pair(b, cost));
+        graph[b].push_back(make_pair(a, cost));
+    }
+
+    return 0;
 }
