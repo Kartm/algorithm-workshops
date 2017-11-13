c21dac9f2ca3260cf3846c60ab9571508d92273b
Łukasz Blachnicki <superkwek0@gmail.com>
2017-11-13 17:21:38 +0100

    It works!

--- School/Fixed-point_number_representation.cpp
+++ School/Fixed-point_number_representation.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include <iomanip>
+#include <cmath>
 
 using namespace std;
 
@@ -8,9 +10,7 @@ vector<int> ReverseVector(vector<int> input)
 {
     vector<int> reversed;
     for(int i = input.size() - 1; i >= 0; i--)
-    {
         reversed.push_back(input[i]);
-    }
     return reversed;
 }
 
@@ -29,26 +29,41 @@ vector<int> BackwardIntegerToBinary(int integer)
     return binaryRepresentation;
 }
 
+pair<int, double> Multiply(double input)
+{
+    double multiplied = input * 2.0;
+    int leftSide = (int)multiplied;
+    double rightSide = multiplied - (double)leftSide;
+    return make_pair(leftSide, rightSide);
+}
+
 int main()
 {
     //ios_base::sync_with_stdio(false);
     //cin.tie(NULL);
+    cout << "Set approximation: ";
+    int approximation; cin >> approximation;
+    cout << "Enter a real number: ";
     double realNumber; cin >> realNumber;
-    int leftSide = (int)realNumber;
-    vector<int> binaryLeftSide = ReverseVector(BackwardIntegerToBinary(leftSide));
 
+
+    int leftSide = (int)realNumber;
+    cout << "Left side in fixed-point representation: ";
+    vector<int> binaryLeftSide = ReverseVector(BackwardIntegerToBinary(leftSide));
+    for(int i = 0; i < binaryLeftSide.size(); i++)
+        cout << binaryLeftSide[i];
+    cout << "\n";
+
+    cout << "Right side in fixed-point representation: ";
     double rightSide = realNumber - (double)leftSide;
-    int approximation = 32;
     for(int i = 0; i < approximation; i++)
     {
-
+        pair<int, double> result = Multiply(rightSide);
+        if(result.first == 1)
+            cout << "1";
+        else
+            cout << "0";
+        rightSide = result.second;
     }
-    //cout << leftSide;
-    //cout << endl << rightSide;
-    /*binary = BackwardIntegerToBinary(test);
-    for(int i = 0; i < binary.size(); i++)
-    {
-        cout << binary[i];
-    }*/
     return 0;
 }
