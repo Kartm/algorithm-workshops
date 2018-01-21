f1557b7861f16e486054fee375ad0ca4f8387513
Lukasz Blachnicki (Ubuntu) <superkwek0@gmail.com>
2018-01-21 01:48:45 +0100

    New beginning.

---
+++ Random programs/Testing_vs_code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Testing_vs_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
