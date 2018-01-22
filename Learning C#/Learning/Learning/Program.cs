b9b18b30407e53660f3aa33b7ed4921821fa89a6
Łukasz Blachnicki <superkwek0@gmail.com>
2018-01-22 07:28:57 +0100

    Let's go.

---
+++ Learning C#/Learning/Learning/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    public interface ITest
+    {
+        void TestRequiredMethod();
+    }
+
+    public class Part
+    {
+        public Part()
+        {
+
+        }
+        public void TestMethod()
+        {
+            Console.WriteLine("TestMethod");
+        }
+    }
+
+    public class PartBig:Part, ITest
+    {
+        public PartBig()
+        {
+
+        }
+
+        public void TestRequiredMethod()
+        {
+            Console.WriteLine("Required method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartBig test = new PartBig();
+            test.TestMethod();
+        }
+    }
+}
